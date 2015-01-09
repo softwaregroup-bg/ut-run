30f3fca8fad9e607982dcc40f4a49996c4fcde67
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-01-09 15:51:24 +0200

    streams and logging support

--- worker.js
+++ worker.js
@@ -1,6 +1,29 @@
 var wire = require('wire');
 
 module.exports = wire({
+    host: '0.0.0.0',
+    port: 30001,
+    console: {
+        create: 'ut-port-console',
+        init: 'init',
+        properties: {
+            config: {
+                host: {$ref: 'host'},
+                port: {$ref: 'port'}
+            }
+        },
+        ready:'start'
+    },
+    socketStream:{
+        create: {
+            module:'ut-log/socketStream',
+            args:{
+                host: {$ref: 'host'},
+                port: {$ref: 'port'},
+                objectMode: true
+            }
+        }
+    },
     log: {
         create: {
             module: 'ut-log',
@@ -11,6 +34,11 @@ module.exports = wire({
                     {
                         level: 'trace',
                         stream: 'process.stdout'
+                    },
+                    {
+                        level: 'trace',
+                        stream: {$ref:'socketStream'},
+                        type: 'raw'
                     }
                 ]
             }
