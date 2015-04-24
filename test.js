22d3e020445d437dccf91da16e9158e1c12accc2
LiveGood <https://LiveGood@github.com>
2015-04-24 15:14:19 +0300

    added test.js file

---
+++ test.js
@@ -0,0 +1,26 @@
+/**
+ * ut-debug module is used to start the switch, like this:
+ require('ut-run/debug').start(
+ require('../impl/<implementation>/server'),
+ require('../impl/<implementation>/config/dev.json')
+ );
+ */
+module.exports = {
+    start: function(impl, config) {
+        require('repl').start({useGlobal: true});
+        require('when/monitor/console');
+        
+        require('wire')({
+            consoleHost: config.console.host,
+            consolePort: config.console.port,
+            serverPort: config.master.serverPort,
+            clientPort: config.master.clientPort,
+            master: require('ut-run/master')
+        }).then(function(context) {
+            context.wire({worker: require('ut-run/worker')}).then(function(context) {
+                app = context;
+                context.worker.run.loadImpl(impl, config);
+            }).done();
+        }).done();
+    }
+}
