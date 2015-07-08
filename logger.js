072b265413a0f2886dbff314e89c7d02286b1a83
stamen.peev <stamen.peev@softwaregroup-bg.com>
2015-07-08 11:19:57 +0300

    define logstreams in implementation config json

--- logger.js
+++ logger.js
@@ -1,4 +1,5 @@
-module.exports={
+var _ = require('lodash');
+module.exports = {
     console: {
         create: 'ut-port-console',
         init: 'init',
@@ -10,33 +11,32 @@ module.exports={
         },
         ready:'start'
     },
-    socketStream:{
-        create: {
-            module:'ut-log/socketStream',
-            args:{
-                host: {$ref: 'config.console.host'},
-                port: {$ref: 'config.console.port'},
-                objectMode: true
-            }
-        }
-    },
     log: {
         create: {
             module: 'ut-log',
             args: {
                 type: 'bunyan',
                 name: 'bunyan_test',
-                streams: [
+                streams: _.union([
                     {
                         level: 'trace',
                         stream: 'process.stdout'
                     },
                     {
                         level: 'trace',
-                        stream: {$ref:'socketStream'},
+                        stream: {
+                            create: {
+                                module:'ut-log/socketStream',
+                                args:{
+                                    host: {'$ref': 'config.console.host'},
+                                    port: {'$ref': 'config.console.port'},
+                                    objectMode: true
+                                }
+                            }
+                        },
                         type: 'raw'
                     }
-                ]
+                ], {$ref: 'config.logStreams'})
             }
         }
     }
