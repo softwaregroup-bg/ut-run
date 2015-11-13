0a9ccc10068a157b21bc49e51d1b7b23fd34aa04
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2015-11-13 08:47:43 +0200

    fix: fix for invoking start methods sequentially and fix environment variables to be uppercase

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var assign = require('lodash/object/assign');
 
 module.exports = {
 
-    bus:null,
-    config:null,
-    logFactory:null,
+    bus: null,
+    config: null,
+    logFactory: null,
 
-    ready:function() {
+    ready: function() {
         this.config = this.config || {};
         if (this.bus) {
             return this.bus.register({
-                run:this.run.bind(this),
+                run: this.run.bind(this)
             });
         }
     },
 
-    loadImpl:function(implementation, config) {
+    loadImpl: function(implementation, config) {
         if (typeof implementation === 'string') {
-            implementation = require (implementation);
+            implementation = require(implementation);
         }
 
         var ports = implementation.ports;
@@ -34,10 +34,10 @@ module.exports = {
         }
 
         if (implementation.validations instanceof Object) {
-            Object.keys(implementation.validations).forEach(function (validationName) {
+            Object.keys(implementation.validations).forEach(function(validationName) {
                 var module = implementation.modules[validationName];
                 var validation = implementation.validations[validationName];
-                module && Object.keys(validation).forEach(function (value) {
+                module && Object.keys(validation).forEach(function(value) {
                     assign(module[value], validation[value]);
                 });
             });
@@ -49,41 +49,41 @@ module.exports = {
                 return all;
             }.bind(this), [])
         ).then(function(contexts) {
-                contexts.forEach(function(context) {
-                    context.port.start();
-                });
+            return when.reduce(contexts, function(prev, context) {
+                return context.port.start();
+            }, []).then(function() {
                 return ports;
-            }
-        )
+            });
+        });
     },
 
-    loadConfig:function(config) {
+    loadConfig: function(config) {
         var Port = (config.createPort instanceof Function) ? config.createPort : require('ut-port-' + config.type);
-        var port = new Port()
+        var port = new Port();
         port.bus = this.bus;
         port.logFactory = this.logFactory;
         assign(port.config, config);
-        return when(port.init()).then(function(){
-            return {port:port};
-        })
+        return when(port.init()).then(function() {
+            return {port: port};
+        });
     },
 
-    run:function() {
+    run: function() {
         if (process.type === 'browser') {
             require('ut-front/electron')(module.parent.filename);
         } else {
             require('babel-core/register')({
-                extensions: [".jsx"]
+                extensions: ['.jsx']
             });
 
             var argv = require('minimist')(process.argv.slice(2));
-            var app = process.env.ut_app || argv._[0] || 'server';
-            var method = process.env.ut_method || argv._[1] || 'debug';
-            var env = process.env.ut_env || argv._[2] || 'dev';
+            var app = process.env.UT_APP || argv._[0] || 'server';
+            var method = process.env.UT_METHOD || argv._[1] || 'debug';
+            var env = process.env.UT_ENV || argv._[2] || 'dev';
             var config = module.parent.require('./' + app + '/' + env + '.json');
             var main = module.parent.require('./' + app);
 
-            if (config.cluster && config.masterBus && config.masterBus.socket && config.masterBus.socket.port){
+            if (config.cluster && config.masterBus && config.masterBus.socket && config.masterBus.socket.port) {
                 var cluster = require('cluster');
                 if (cluster.isMaster) {
                     var workerCount = config.cluster.workers || require('os').cpus().length;
@@ -96,7 +96,7 @@ module.exports = {
                     config.console && config.console.port && (config.console.port = config.console.port + cluster.worker.id);
                 }
             }
-            return require('ut-run/' + method).start(main,config);
+            return require('ut-run/' + method).start(main, config);
         }
     }
 
