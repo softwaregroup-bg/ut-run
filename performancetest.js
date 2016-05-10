0d82f3532a85d74b5903873c00029fa209da3794
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2016-05-10 09:18:38 +0300

    clean lint errors

--- performancetest.js
+++ performancetest.js
@@ -7,23 +7,30 @@ var tape = require('blue-tape');
 var run = require('./index').run;
 var loadtest = require('loadtest');
 
-function loadtest_test(params, assert, bus, flow) {
+function loadTest(params, assert, bus, flow) {
     var step = flow.shift();
     var start = Date.now();
 
-    var passed = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'p', 'Passed tests');
-    var duration = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'd', 'Test duration');
-    var totalRequests = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'TotalRequests', 'Total requests');
-    var totalErrors = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'TotalErrors', 'Total errors');
-    var rps = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'RpS', 'Requests per seconds');
-    var meanLatencyMs = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'MeanLatencyMS', 'Mean latency');
-    var maxLatencyMs = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'MaxLatencyMs', 'Max latency');
+    var passed = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'p', 'Passed tests');
+    var duration = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'd', 'Test duration');
+    var totalRequests = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'TotalRequests', 'Total requests');
+    var totalErrors = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'TotalErrors', 'Total errors');
+    var rps = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'RpS', 'Requests per seconds');
+    var meanLatencyMs = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'MeanLatencyMS', 'Mean latency');
+    var maxLatencyMs = params.name && bus.performance &&
+        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'MaxLatencyMs', 'Max latency');
 
     var state = true;
     if (step.inPerformanceTest) {
         var httpSettings = {
             url: params.url,
-            body: Object.assign({ method: step.method, params: step.params }, params.body || { jsonrpc: '2.0', 'id': 1 }),
+            body: Object.assign({method: step.method, params: step.params}, params.body || {jsonrpc: '2.0', 'id': 1}),
             method: params.method || 'POST',
             contentType: params.contentType || 'application/json',
             maxRequests: params.maxRequests || 10, // max requests per api call for the whole test
@@ -35,9 +42,9 @@ function loadtest_test(params, assert, bus, flow) {
                 state = state && assert._ok && result;
             }
         };
-        return new Promise(function(resolve) {
+        return new Promise(function(resolve, reject) {
             loadtest.loadTest(httpSettings, function(error, result) {
-                resolve(result);
+                error ? reject(error) : resolve(result);
             });
         }).then(function(result) {
             duration && duration(Date.now() - start);
@@ -48,18 +55,19 @@ function loadtest_test(params, assert, bus, flow) {
             meanLatencyMs && meanLatencyMs(result.meanLatencyMs);
             maxLatencyMs && maxLatencyMs(result.maxLatencyMs);
 
-            var metrics = { stepName: step.name, method: step.method };
+            var metrics = {stepName: step.name, method: step.method};
 
             if (result.errorCodes) {
                 var keys = Object.keys(result.errorCodes);
                 keys.map(function(key) {
-                    var errorCode = params.name && bus.performance && bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'ErrorCode' + key, 'Error code ' + key);
+                    var errorCode = params.name && bus.performance &&
+                        bus.performance.register(bus.config.implementation + '_test_' + params.name, 'gauge', 'ErrorCode' + key, 'Error code ' + key);
                     errorCode(result.errorCodes[key]);
                 });
             }
             bus.performance && bus.performance.write(metrics);
             if (flow.length) {
-                loadtest_test(params, assert, bus, flow);
+                loadTest(params, assert, bus, flow);
             } else {
                 setTimeout(() => {
                     bus.performance.stop();
@@ -68,7 +76,7 @@ function loadtest_test(params, assert, bus, flow) {
         });
     } else {
         if (flow.length) {
-            loadtest_test(params, assert, bus, flow);
+            loadTest(params, assert, bus, flow);
         }
     }
 }
@@ -81,6 +89,6 @@ module.exports = function(params) {
     };
     var clientRun = run(client);
     tape('Performance test start', (assert) => clientRun.then((client) => {
-        params.steps(assert, client.bus, loadtest_test.bind(null, params));
+        params.steps(assert, client.bus, loadTest.bind(null, params));
     }));
 };
