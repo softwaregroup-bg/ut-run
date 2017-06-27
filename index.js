38c9b181515a878de11faf1a54687c693230b899
Kanagu Duraiswamy <Kanagu.Duraiswamy@softwaregroup-bg.com>
2017-06-27 22:21:05 +1000

    feat: UIS-2550 Renamed the config parameter from utersion to just version.

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ module.exports = {
         } else {
             var config = params.config;
             if (!config) {
-                config = {params: {}, runMaster: true, runWorker: true, utVersion: params.utVersion};
+                config = {params: {}, runMaster: true, runWorker: true, version: params.version};
                 var argv = require('minimist')(process.argv.slice(2));
                 var busMode = process.env.UT_BUS_MODE || params.busMode;
                 if (busMode === 'master') {
