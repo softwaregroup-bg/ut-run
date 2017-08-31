3514a27dc9fb2e61a0cd65e398c89e3fba3184b5
stambata <stamen.peev@softwaregroup-bg.com>
2017-09-01 01:24:01 +0300

    feat: add registry dynamically if the respective configuration has been specified

--- index.js
+++ index.js
@@ -34,10 +34,22 @@ module.exports = {
                 return prev;
             }, {ports: [], modules: {}, validations: {}});
         }
-
-        var ports = implementation.ports;
-        var portsStarted = [];
         config = config || {};
+        var ports = [];
+        if (config.registry) {
+            ports.push({
+                id: 'registry',
+                createPort: require('ut-port-registry'),
+                context: {
+                    version: config.version,
+                    impl: config.implementation
+                }
+            });
+        }
+        if (Array.isArray(implementation.ports)) {
+            ports.push.apply(ports, implementation.ports);
+        }
+        var portsStarted = [];
         this.bus.config = config;
 
         if (implementation.modules instanceof Object) {
@@ -142,7 +154,7 @@ module.exports = {
                 config.params.app = process.env.UT_APP || params.app || argv._[0] || 'server';
                 config.params.method = process.env.UT_METHOD || params.method || argv._[1] || 'debug';
                 config.params.env = process.env.UT_ENV || params.env || argv._[2] || 'dev';
-                config = Object.assign(config, parent.require('./' + config.params.app + '/' + config.params.env));
+                Object.assign(config, parent.require('./' + config.params.app + '/' + config.params.env));
             }
             var main = params.main || parent.require('./' + config.params.app);
 
