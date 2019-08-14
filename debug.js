9963e8ad51b4799bc0b0952d9538cabf2c3f3a39
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-08-14 07:58:21 +0300

    fix: elasticsearch indexing

--- debug.js
+++ debug.js
@@ -89,7 +89,7 @@ module.exports = {
             log = logFactory.createLog((mergedConfig && mergedConfig.run && mergedConfig.run.logLevel) || 'info', {name: 'run', context: 'run'});
             log && log.info && log.info({
                 $meta: {mtid: 'event', opcode: 'run.debug'},
-                config: mergedConfig.config,
+                config: {path: mergedConfig.config},
                 runMaster: mergedConfig.runMaster,
                 runWorker: mergedConfig.runWorker,
                 repl: mergedConfig.repl
