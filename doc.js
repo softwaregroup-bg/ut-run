3ac31e6456f5526ad53b649a2cddb8d0f093a073
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-01-07 17:43:02 +0200

    fix: require json-schema-to-markdown conditionally

--- doc.js
+++ doc.js
@@ -1,5 +1,4 @@
 const create = require('./create');
-const parse = require('json-schema-to-markdown');
 const fs = require('fs');
 const path = require('path');
 module.exports = async function doc(serviceConfig) {
@@ -8,7 +7,7 @@ module.exports = async function doc(serviceConfig) {
         utBus: { serviceBus: { jsonrpc: true } }
     });
     const [port] = await service.create([() => ({doc: [serviceConfig]})]);
-    const md = parse(port.configSchema);
+    const md = require('json-schema-to-markdown')(port.configSchema);
     fs.writeFileSync(path.join(process.cwd(), 'config.md'), md);
     process.exit(0); // eslint-disable-line no-process-exit
 };
