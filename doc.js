41d610214c149b6daa8b629e59293753c65e29d3
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2020-01-14 21:40:18 +0200

    fix: generate linable markdown

--- doc.js
+++ doc.js
@@ -2,12 +2,14 @@ const create = require('./create');
 const fs = require('fs');
 const path = require('path');
 module.exports = async function doc(serviceConfig) {
-    const {service} = await create({
+    const {service, serviceBus} = await create({
         implementation: 'doc',
+        repl: false,
+        utLog: { streams: { udp: false } },
         utBus: { serviceBus: { jsonrpc: true } }
     });
     const [port] = await service.create([() => ({doc: [serviceConfig]})]);
-    const md = require('json-schema-to-markdown')(port.configSchema);
-    fs.writeFileSync(path.join(process.cwd(), 'config.md'), md);
-    process.exit(0); // eslint-disable-line no-process-exit
+    const md = require('json-schema-to-markdown')({title: 'Configuration', ...port.configSchema});
+    fs.writeFileSync(path.join(process.cwd(), 'config.md'), md + '\n');
+    await serviceBus.stop();
 };
