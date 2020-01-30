e0ee5725d2eac311ece240016a8ad7037f5d0e2f
stambata <stamen.peev@softwaregroup-bg.com>
2020-01-30 14:34:52 +0200

    test: allow passing jobs in unit tests (#78)

--- unit.js
+++ unit.js
@@ -6,7 +6,8 @@ module.exports = function unit(serviceConfig, envConfig, assert, vfs) {
         return [].concat(serviceConfig);
     };
 
-    const { steps } = envConfig;
+    const { jobs, steps } = envConfig;
+    delete envConfig.jobs;
     delete envConfig.steps;
     delete envConfig.params;
 
@@ -28,7 +29,9 @@ module.exports = function unit(serviceConfig, envConfig, assert, vfs) {
         ...envConfig
     };
 
-    if (typeof steps === 'undefined') return require('./debug')(server, serverConfig, assert, vfs);
+    if (typeof jobs === 'undefined' && typeof steps === 'undefined') {
+        return require('./debug')(server, serverConfig, assert, vfs);
+    }
 
     return require('./test')({
         type: 'unit',
@@ -37,7 +40,7 @@ module.exports = function unit(serviceConfig, envConfig, assert, vfs) {
         client: false,
         server,
         serverConfig,
-        jobs: [{
+        jobs: jobs || [{
             name: 'port.test',
             steps: function test(test, bus, run) {
                 return typeof steps === 'function' ? steps(test, bus) : run(test, bus, steps);
