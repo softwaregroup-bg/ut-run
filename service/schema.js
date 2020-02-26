f364b5a508b9fe3bdf92ebdd7809c4ee8759e821
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-02-26 14:04:20 +0200

    feat: pass config as formData when loading install form

--- service/schema.js
+++ service/schema.js
@@ -18,35 +18,35 @@ const dropReadOnly = param => {
     return param;
 };
 
-const set = (object, property, schema, uiSchema) => {
-    if (property && schema) {
-        const editable = dropReadOnly(schema);
-        if (editable && (!editable.properties || Object.keys(editable.properties).length)) {
-            property.split('.').reduce((prev, name, index, array) => {
-                const value = (index === array.length - 1) ? editable : {type: 'object', properties: {}};
-                prev.properties = prev.properties || {};
-                prev.properties[name] = prev.properties[name] || value;
-                return prev.properties[name];
-            }, object.schema);
-        }
-    }
-    if (property && uiSchema) {
-        property.split('.').reduce((prev, name, index, array) => {
-            const value = (index === array.length - 1) ? uiSchema : {};
+const set = (object, config, schema, uiSchema) => {
+    if (!schema || !config || !config.id) return;
+    const editable = dropReadOnly(schema);
+    if (editable && (!editable.properties || Object.keys(editable.properties).length)) {
+        const tokens = config.id.split('.');
+        tokens.reduce((prev, name, index, array) => {
+            const value = (index === array.length - 1) ? editable : {type: 'object', properties: {}};
+            prev.properties = prev.properties || {};
+            prev.properties[name] = prev.properties[name] || value;
+            return prev.properties[name];
+        }, object.schema);
+        tokens.reduce((prev, name, index, array) => {
+            const value = (index === array.length - 1) ? config : {};
             prev[name] = prev[name] || value;
             return prev[name];
-        }, object.uiSchema);
+        }, object.formData);
+        if (uiSchema) {
+            tokens.reduce((prev, name, index, array) => {
+                const value = (index === array.length - 1) ? uiSchema : {};
+                prev[name] = prev[name] || value;
+                return prev[name];
+            }, object.uiSchema);
+        }
     }
 };
 
-module.exports = ({portsAndModules, log, schema, uiSchema = {}}) => {
-    return portsAndModules.reduce((prev, portOrModule) => {
-        set(
-            prev,
-            portOrModule.config && portOrModule.config.id,
-            portOrModule.configSchema,
-            portOrModule.configUiSchema
-        );
+module.exports = ({portsAndModules, log, schema, uiSchema = {}, formData = {}}) => {
+    return portsAndModules.reduce((prev, {config, configSchema, configUiSchema}) => {
+        set(prev, config, configSchema, configUiSchema);
         return prev;
     }, {
         schema: {
@@ -55,6 +55,7 @@ module.exports = ({portsAndModules, log, schema, uiSchema = {}}) => {
             type: 'object',
             ...schema
         },
-        uiSchema
+        uiSchema,
+        formData
     });
 };
