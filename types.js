84715a062c92a87ad6ce8720ee83565a20507918
Kalin Krustev <kalin.krustev@gmail.com>
2022-08-25 18:51:17 +0300

    chore: tests types

--- types.js
+++ types.js
@@ -93,8 +93,9 @@ interface methods extends ${dep.replace(/-/g, '')}.handlers {}
 export type libFactory = ut.libFactory<methods, errors>
 export type handlerFactory = ut.handlerFactory<methods, errors, handlers<'local'>>
 export type handlerSet = ut.handlerSet<methods, errors, handlers<'local'>>
+export type test = ut.test<keyof methods | keyof handlers>
 
-import portal from 'ut-portal/handlers'
+import portal from 'ut-portal'
 export type pageFactory = portal.pageFactory<methods, errors>
 export type pageSet = portal.pageSet<methods, errors>
 `);
