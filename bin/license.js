3b0f0ac30c39a1010b3b60a7dab8b0c5512f0f40
Stamen Peev <stamen.peev@softwaregroup-bg.com>
2024-07-10 16:28:52 +0300

    chore: align license environmanet variables in accordance to latest impl-tools version

--- bin/license.js
+++ bin/license.js
@@ -17,7 +17,10 @@ module.exports = async function license(serviceConfig, envConfig, assert, vfs) {
             repository
         });
 
-        process.env.AEGIS_BUILD = JSON.stringify(project.publicKey);
+        process.env.AEGIS_BUILD = 1;
+        process.env.AEGIS_KEY = project.encryptionKey;
+        process.env.AEGIS_IV = project.encryptionIV;
+        process.env.AEGIS_CIPHER = project.encryptionCipher;
     } catch (e) {
         serviceBus.log.error(e);
     }
